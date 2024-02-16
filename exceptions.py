559265d9bb924db3be4e13d237678a4c346993f3
brix john novesteras <brixjohnovesteras@gmail.com>
2024-02-16 13:14:35 +0800

    exceptions

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+# x = 333
+
+# if x > 100:
+#     raise Exception ("x is  bigger than 100")
+
+# a = "s"
+# try:
+#     print(a)
+# except:
+#     print("variable is not defined")
+
+# for i in range(6):
+#     print("Im so happy!")
+
+# try:
+#     print(12*6)
+# except:
+#     print("This operation can't be performed")
+# else:
+#     print("This operation can be performed")
+
+## ASSERTION ERROR
+best_burger = "Burger King"
+num_2_burger = "McDonalds"
+
+assert best_burger ==  "Burger King"
+assert num_2_burger == "McDonaldss"
