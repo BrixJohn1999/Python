bc5ad88debd85bb1bf657e4957988aa0c1d2e879
brix john novesteras <brixjohnovesteras@gmail.com>
2024-02-16 13:53:26 +0800

    coding excercise for calling modules

---
+++ functionfile.py
@@ -0,0 +1,8 @@
+def general_greeting():
+    print("Hello, Everyone!")
+
+def personal_greeting(name):
+    print("Hi, " + name + "!")
+
+def your_province(province):
+    print("Oh, I heard " +province + "is a nice place!")
---
+++ functionmain.py
@@ -0,0 +1,5 @@
+from functionfile import personal_greeting
+from functionfile import your_province
+
+personal_greeting("Brix")
+your_province("Davao Del Norte Tagum")
