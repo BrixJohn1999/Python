aad15a130a6c2db15d7a24e33b7de89c243a5ee4
brix john novesteras <brixjohnovesteras@gmail.com>
2024-02-16 13:46:23 +0800

    importing modules

---
+++ main.py
@@ -0,0 +1,13 @@
+import module1 as m1 
+
+sum1 = m1.get_sum(5,10)
+print("result: ", sum1)
+
+quotient = m1.get_quotient(5, 10)
+print("result:",quotient)
+
+peps1 = m1.user_1
+peps2 = m1.user_2
+
+print("Hello! " + peps1.name + ", Age:",peps1.age, "From: " + peps1.country)
+print("Hello! " + peps2.name + ", Age:",peps2.age, "From: " + peps2.country)
---
+++ module1.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
+
+user_1 = User("Brix", 24, "Philippines") 
+user_2 = User("Priya", 25, "India")
+
+def get_sum(x,y):
+    return x + y
+
+def get_quotient(x,y):
+    return x / y
